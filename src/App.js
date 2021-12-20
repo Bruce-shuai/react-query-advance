638959bc58e9bb0d713b203350252e0453a22f7c
帅得乱七八糟 <284970670@qq.com>
2021-12-20 08:44:12 +0800

    建立react-query, 使用useQuery代替useEffect&useState 获取异步数据

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import HomePage from './components/Home.page';
 import RQSuperHeros from './components/RQSuperHeros.page';
 import SuperHeros from './components/SuperHeros.page';
+import { QueryClientProvider, QueryClient } from 'react-query';
 import {
   BrowserRouter as Router,
   Routes,
@@ -9,30 +10,36 @@ import {
   Link
 } from "react-router-dom";
 
+const queryClient = new QueryClient();   // 在组件外创建实例
+
 function App() {
+  // 创建一个queryclient的实例
+
   return (
-    <Router>
-      <>
-        <nav>
-            <ul>
-              <li>
-                <Link to='/'>Home</Link>
-              </li>
-              <li>
-                <Link to='/super-heroes'>Traditional Super Heroes</Link>
-              </li>
-              <li>
-                <Link to='/rq-super-heroes'>RQ Super Heroes</Link>
-              </li>
-            </ul>
-          </nav>
-        <Routes>
-          <Route path="/" element={<HomePage />} />
-          <Route path="super-heroes" element={<SuperHeros />} />
-          <Route path="rq-super-heroes" element={<RQSuperHeros />} />
-        </Routes>
-      </>
-    </Router>
+    <QueryClientProvider client={queryClient}>
+      <Router>
+        <>
+          <nav>
+              <ul>
+                <li>
+                  <Link to='/'>Home</Link>
+                </li>
+                <li>
+                  <Link to='/super-heroes'>Traditional Super Heroes</Link>
+                </li>
+                <li>
+                  <Link to='/rq-super-heroes'>RQ Super Heroes</Link>
+                </li>
+              </ul>
+            </nav>
+          <Routes>
+            <Route path="/" element={<HomePage />} />
+            <Route path="super-heroes" element={<SuperHeros />} />
+            <Route path="rq-super-heroes" element={<RQSuperHeros />} />
+          </Routes>
+        </>
+      </Router>
+    </QueryClientProvider>
   );
 }
 
--- src/components/RQSuperHeros.page.js
+++ src/components/RQSuperHeros.page.js
@@ -1,9 +1,32 @@
 import React from 'react'
+import { useQuery } from 'react-query';
+import axios from 'axios';
 
 export default function RQSuperHerosPage() {
+  // useQuery 至少需要两个参数：
+  // param1 必须是一个独一无二的key，用于识别这个query
+  // param2 接受一个函数，并且该函数返回的是一个promise
+
+  // 返回值：是一个对象，该对象包含针对这个super-heroes查询的所有你需要的信息
+  const result = useQuery('super-heroes', () => {
+    return axios.get('http://localhost:3004/superheroes')
+  });
+  console.log('result', result);
+  const { isLoading, data } = result;
+
+  // 可以见到，有了useQuery就不再需要使用useEffect + useState来获取api接口数据了
+  if (isLoading) {
+    return <h2>Loading...</h2>
+  }
   return (
-    <div>
-      RQSuperHeros
-    </div>
+    <>
+      <h2>RQ Super Heroes Page</h2>
+      {
+        data?.data.map(hero => {
+          return <div key={hero.id}>{hero.name}</div>
+        })
+      }
+    </>
   )
 }
+  
