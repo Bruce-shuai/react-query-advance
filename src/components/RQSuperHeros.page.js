63ced33926c0b68bbfffa16f44d0518c66afaf60
帅得乱七八糟 <284970670@qq.com>
2021-12-20 11:08:51 +0800

    指定事件发送请求 enable:false & refetch

--- src/components/RQSuperHeros.page.js
+++ src/components/RQSuperHeros.page.js
@@ -11,14 +11,14 @@ export default function RQSuperHerosPage() {
   const result = useQuery('super-heroes', () => {
     return axios.get('http://localhost:3004/superheroes')
   }, {
-    refetchInterval: 200,   // 默认值是false，这里设置2000 表示每2s就会发送一次网络请求（更新一次数据）
-    refetchIntervalInBackground: false,  // 默认是false，结合上面的refetchInterval一起使用，表示当你离开这个窗口页面，比如浏览其他网页的时候，你设置false，则该页面的网络请求会暂停发送
+    // 当点击按钮时，发送请求
+    enabled: false,  // 第一步：禁止请求的发送
   });
   
-  const { isLoading, isError, data, error, isFetching } = result;
-
-  console.log({isLoading, isFetching});  // 在10s内，来回切换对应路由，isFetching 也是false
-  if (isLoading) {
+  // 第二步：获取 refetch  方法
+  const { isLoading, isError, data, error, isFetching, refetch } = result;
+  console.log({isLoading, isFetching});
+  if (isLoading || isFetching) {
     return <h2>Loading...</h2>
   }
   if (isError) {
@@ -27,6 +27,7 @@ export default function RQSuperHerosPage() {
   return (
     <>
       <h2>RQ Super Heroes Page</h2>
+      <button onClick={refetch}>获取Heros</button>
       {
         data?.data.map(hero => {
           return <div key={hero.id}>{hero.name}</div>
