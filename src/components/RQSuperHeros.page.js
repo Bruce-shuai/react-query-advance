d670b24ef67ce3719cf5a7a64a8cf1ad02c11df7
帅得乱七八糟 <284970670@qq.com>
2021-12-20 09:36:49 +0800

    react query 缓存及缓存时间配置

--- src/components/RQSuperHeros.page.js
+++ src/components/RQSuperHeros.page.js
@@ -6,15 +6,17 @@ export default function RQSuperHerosPage() {
   // useQuery 至少需要两个参数：
   // param1 必须是一个独一无二的key，用于识别这个query
   // param2 接受一个函数，并且该函数返回的是一个promise
-
+  // param3 是用户自定义配置 query
   // 返回值：是一个对象，该对象包含针对这个super-heroes查询的所有你需要的信息
   const result = useQuery('super-heroes', () => {
     return axios.get('http://localhost:3004/superheroes')
+  }, {
+    cacheTime: 5000,    // 设置缓存时间为5s, 当你转到其他路由页面5s后，缓存失效(useQuery 默认cacheTime:5 mins)
   });
-  console.log('result', result);
-  const { isLoading, isError, data, error } = result;
+  const { isLoading, isError, data, error, isFetching } = result;
 
-  // 可以见到，有了useQuery就不再需要使用useEffect + useState来获取api接口数据了
+  // 即使对应接口的后台数据更新，isLoading也是false，在更新的数据返回之时，会无形之间把ui的数据显示发生改变。而不会有loading这种界面出现
+  console.log({isLoading, isFetching});
   if (isLoading) {
     return <h2>Loading...</h2>
   }
