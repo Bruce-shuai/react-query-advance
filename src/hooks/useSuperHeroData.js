baf864887da2c29d2c5c62999dac7774be52fe5e
帅得乱七八糟 <284970670@qq.com>
2021-12-21 08:49:48 +0800

    使用useQueryClient + initialData 减少数据请求的次数

--- src/hooks/useSuperHeroData.js
+++ src/hooks/useSuperHeroData.js
@@ -1,4 +1,4 @@
-import { useQuery } from 'react-query';
+import { useQuery, useQueryClient } from 'react-query';
 import axios from 'axios';
 
 
@@ -16,6 +16,22 @@ const fetchSuperHero = ({queryKey}) => {
   return axios.get(`http://localhost:3004/superheroes/${queryKey[1]}`)
 }
 
+// 提供 initial query data
 export const useSuperHeroData = (heroId) => {
-  return useQuery(['super-hero', heroId], fetchSuperHero);
+  const queryClient = useQueryClient();
+  // 如果在请求这个数据之前就已经获得了一系列hero的数据即heroes的数据。那我可以直接用heroes的数据而不是又去网络请求
+  return useQuery(['super-hero', heroId], fetchSuperHero, {
+    initialData: () => {
+      const hero = queryClient
+        .getQueryData('super-heroes')
+        ?.data?.find(hero => hero.id === parseInt(heroId))
+
+        if (hero) {
+          return {
+            data: hero
+          }} else {
+            return undefined
+          }      
+    }
+  });
 }
