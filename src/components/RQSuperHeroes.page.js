bbcc0c150c601c2d4cedc42ab8c53a94a5dbe7c7
帅得乱七八糟 <284970670@qq.com>
2021-12-21 15:11:15 +0800

    使用useMutation发送post请求 以及refetch的使用

--- src/components/RQSuperHeroes.page.js
+++ src/components/RQSuperHeroes.page.js
@@ -1,7 +1,9 @@
-import React from 'react';
+import React, {useState} from 'react';
 import { Link } from 'react-router-dom';
-import { useSuperHeroesData } from '../hooks/useSuperHeroesData';
+import { useSuperHeroesData, useAddSuperHeroData } from '../hooks/useSuperHeroesData';
 export default function RQSuperHerosPage() {
+  const [name, setName] = useState('');
+  const [alterEgo, setAlterEgo] = useState('');
 
   const onSuccess = (data) => {
     // 这里的数据，获取的是 接口请求返回的数据
@@ -12,19 +14,44 @@ export default function RQSuperHerosPage() {
   }
 
   const result = useSuperHeroesData(onSuccess, onError);
+  // mutate 是一个函数，我们可以调用这个函数进行post请求
+  const { mutate:addHero } = useAddSuperHeroData();  // 不传任何参数
 
-  const { isLoading, isError, data, error, isFetching } = result;
+  const { isLoading, isError, data, error, isFetching, refetch } = result;
   console.log({isLoading, isFetching});
+
   if (isLoading || isFetching) {
     return <h2>Loading...</h2>
   }
   if (isError) {
     return <h2>{error.message}</h2>
+  } 
+
+  const handleAddHeroClick = () => {
+    console.log({name, alterEgo});
+    const hero = { name, alterEgo }
+    addHero(hero);
   }
-  console.log('data', data);
+
   return (
     <>
       <h2>RQ Super Heroes Page</h2>
+      {/* 这里没有用form表格，而是用的普通div标签 */}
+      <div>   
+        <input 
+          type="text"
+          value={name}
+          onChange={e => setName(e.target.value)}
+        />
+        <input
+          type="text"
+          value={alterEgo}
+          onChange={e => setAlterEgo(e.target.value)}
+        />
+        <button onClick={handleAddHeroClick}>Add Hero</button>
+      </div>
+      {/* 这个refetch 是一个什么操作？直接发送新请求 然后刷新页面 */}
+      <button onClick={refetch}>Fetch heroes</button>
       {
         data?.data.map(hero => {
           return <div key={hero.id}>
--- src/hooks/useSuperHeroesData.js
+++ src/hooks/useSuperHeroesData.js
@@ -1,6 +1,6 @@
 // 自定义这个hooks，(相当于封装了一个特定的useQuery)可以在各个组件中使用，非常方便全局控制
 
-import { useQuery } from 'react-query';
+import { useQuery, useMutation } from 'react-query';
 import axios from 'axios';
 
 export const useSuperHeroesData = (onSuccess, onError) => {
@@ -10,4 +10,13 @@ export const useSuperHeroesData = (onSuccess, onError) => {
     onSuccess: onSuccess,
     onError: onError,
   });
+}
+
+// useMutaion 可以支持delete patch post put 请求
+export const useAddSuperHeroData = () => {
+  // useMutation 不像 useQuery 需要一个key值
+  // param1 一个回调函数
+  return useMutation((hero) => {
+    return axios.post('http://localhost:3004/superheroes', hero)
+  })
 }
